d38de2217f1c3d522e4d4a22ff62fa9e00a31c07
kdchambers <kd.chambers97@gmail.com>
2023-10-26 21:13:49 -0500

    shaders: Failed attempt to compile hlsl with glslang

--- shaders/compile_shaders.bat
+++ shaders/compile_shaders.bat
@@ -1,5 +1,10 @@
 @echo off
+
 dxc.exe -spirv -T vs_6_0 -E main VertexShader.hlsl -Fo .\VertexShader.spv
 dxc.exe -spirv -T ps_6_0 -E main PixelShader.hlsl -Fo .\PixelShader.spv
+
+glslang -V -D --target-env vulkan1.2 -e main -S vert VertexShader.hlsl -o VertexShader.spv
+glslang -V -D --target-env vulkan1.2 -e main -S frag PixelShader.hlsl -o PixelShader.spv
+
 echo 'Done'
 @echo on
